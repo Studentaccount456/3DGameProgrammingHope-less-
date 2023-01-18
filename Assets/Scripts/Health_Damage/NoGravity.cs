0d0c3a684ad9c7f007511bfa14a1b412690d2941
Jan Cuypers <s127806@ap.be>
2023-01-18 16:01:46 +0100

    Final Version Level 4
    Final Version of Level 4.

---
+++ Assets/Scripts/Health_Damage/NoGravity.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoGravity : MonoBehaviour
+{
+    // Reference to PlayerMovement script
+    PlayerMovement playerMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Grabs the gravity variable of playerMovement script and sets it to 0:
+        playerMovement.gravity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Health_Damage/Shoot.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    float timer = 10f;
+    bool start = false;
+    public float shootRate = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float shoot = Input.GetAxis("Fire1");
+        print(shoot);
+
+        if (shoot == 1 && timer >= shootRate) //shoot {
+        {
+            GameObject newProjectile = Instantiate(projectile, transform.position + transform.forward, transform.rotation);
+
+            newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * 5, ForceMode.VelocityChange);
+            start = true;
+            timer = 0f;
+        }
+
+        if (start)
+        {
+            if (timer < shootRate)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                timer = shootRate;
+                start = false;
+            }
+        }
+    }
+}
